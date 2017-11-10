042de8bb109b0fe1b1521ca8bf4d5a5a75ff4e03
maxiao <190572093@qq.com>
2017-11-10 13:43:47 +0800

    soul

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,12 +2,21 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.hencoder.hencoderpracticedraw1.DensityUtil;
+import com.hencoder.hencoderpracticedraw1.R;
+
 public class Practice2DrawCircleView extends View {
 
+    private int circleRadius = 160;
+    private int padding = 25;
+
+    Paint paint = new Paint();
+    
     public Practice2DrawCircleView(Context context) {
         super(context);
     }
@@ -24,6 +33,27 @@ public class Practice2DrawCircleView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(getResources().getColor(R.color.black));
+        canvas.drawCircle(getWidth()/2-circleRadius-padding,getHeight()/2-circleRadius-padding,circleRadius,paint);
+
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(DensityUtil.dip2px(getContext(),1));
+        paint.setColor(getResources().getColor(R.color.black));
+        canvas.drawCircle(getWidth()/2+circleRadius+padding,getHeight()/2-circleRadius-padding,circleRadius,paint);
+
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(getResources().getColor(R.color.blue));
+        canvas.drawCircle(getWidth()/2-circleRadius-padding,getHeight()/2+circleRadius+padding,circleRadius,paint);
+
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(DensityUtil.dip2px(getContext(),20));
+        paint.setColor(getResources().getColor(R.color.black));
+        canvas.drawCircle(getWidth()/2+circleRadius+padding,getHeight()/2+circleRadius+padding,circleRadius,paint);
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
     }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,11 +2,18 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.hencoder.hencoderpracticedraw1.DensityUtil;
+import com.hencoder.hencoderpracticedraw1.R;
+
 public class Practice3DrawRectView extends View {
+
+    private Paint paint = new Paint();
+    private Integer length = DensityUtil.dip2px(getContext(),130);
 
     public Practice3DrawRectView(Context context) {
         super(context);
@@ -23,7 +30,9 @@ public class Practice3DrawRectView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(getResources().getColor(R.color.black));
+        canvas.drawRect( (getWidth()-length)/2,(getHeight()-length)/2,(getWidth()+length)/2,(getHeight()+length)/2,paint);
 //        练习内容：使用 canvas.drawRect() 方法画矩形
     }
 }
